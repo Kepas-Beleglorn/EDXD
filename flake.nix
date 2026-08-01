cde93c9476c15fa01d021ea4c677d3e2b2cb6b03
zer0se7ense7en <49796909+zer0se7ense7en@users.noreply.github.com>
2026-08-01 14:37:03 +0200

    removed hash for debugging purposes

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
                     owner = "wxWidgets";
                     repo = "Phoenix";
                     rev = "wxPython-4.2.4";
-                    hash = "sha256-44e836d1bccd99c38790bb034b6ecf70d9060f6734320560f7c4b0d006144793";
+                    hash = "";
                   };
                   # wxPython 4.2.5 from GitHub source may need build flags
                   # Usually not needed for Phoenix, but kept for safety
