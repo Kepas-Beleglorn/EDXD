b9c237fe12d4c32ab776c13fdedbb091b7e20957
Stian Myklebostad <stian.myklebostad@mittanbud.no>
2026-08-21 10:22:59 +0200

    refactor(nix): Simplify default.nix by returning derivation directly

--- default.nix
+++ default.nix
@@ -1,67 +1,66 @@
 { pkgs, python, src }:
 let
   version = (builtins.fromTOML (builtins.readFile ./pyproject.toml)).project.version;
-  my-python-pkg = python.pkgs.buildPythonPackage {
-    pname = "ed-eXploration-dashboard";
-    inherit version src;
-
-    preBuild = ''
-      echo 'VERSION = "${version}"' > EDXD/_version.py
-    '';
-
-    doCheck = false;
-
-    pythonRelaxDeps = [ "tomli" "filelock" ];
-    nativeBuildInputs = [ python.pkgs.pythonRelaxDepsHook ];
-
-    pyproject = true;
-    build-system = [
-      python.pkgs.setuptools
-      python.pkgs.wheel
-    ];
-
-    buildInputs = [
-      # Wayland / GTK stack
-      pkgs.wayland
-      pkgs.libxkbcommon
-      pkgs.gtk3
-      pkgs.glib
-      pkgs.nss
-      pkgs.nspr
-      pkgs.cairo
-      pkgs.pango
-      pkgs.harfbuzz
-
-      # X11 / XCB stack
-      pkgs.xorg.libX11
-      pkgs.xorg.libXcursor
-      pkgs.xorg.libXrandr
-      pkgs.xorg.libXi
-      pkgs.xorg.libXrender
-      pkgs.xorg.libXext
-      pkgs.xorg.libXfixes
-      pkgs.xorg.libxcb
-      pkgs.xorg.xcbutil
-      pkgs.xorg.xcbutilimage
-      pkgs.xorg.xcbutilkeysyms
-      pkgs.xorg.xcbutilwm
-
-      # zlib (libz.so.1)
-      pkgs.zlib
-
-      # Requirements
-      python.pkgs.tomli
-      python.pkgs.watchdog
-      python.pkgs.wxpython
-      python.pkgs.filelock
-    ];
-
-    propagatedBuildInputs = [
-      python.pkgs.tomli
-      python.pkgs.watchdog
-      python.pkgs.wxpython
-      python.pkgs.filelock
-    ];
-  };
 in
-  my-python-pkg
+python.pkgs.buildPythonPackage {
+  pname = "ed-eXploration-dashboard";
+  inherit version src;
+
+  preBuild = ''
+    echo 'VERSION = "${version}"' > EDXD/_version.py
+  '';
+
+  doCheck = false;
+
+  pythonRelaxDeps = [ "tomli" "filelock" ];
+  nativeBuildInputs = [ python.pkgs.pythonRelaxDepsHook ];
+
+  pyproject = true;
+  build-system = [
+    python.pkgs.setuptools
+    python.pkgs.wheel
+  ];
+
+  buildInputs = [
+    # Wayland / GTK stack
+    pkgs.wayland
+    pkgs.libxkbcommon
+    pkgs.gtk3
+    pkgs.glib
+    pkgs.nss
+    pkgs.nspr
+    pkgs.cairo
+    pkgs.pango
+    pkgs.harfbuzz
+
+    # X11 / XCB stack
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcursor
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXi
+    pkgs.xorg.libXrender
+    pkgs.xorg.libXext
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libxcb
+    pkgs.xorg.xcbutil
+    pkgs.xorg.xcbutilimage
+    pkgs.xorg.xcbutilkeysyms
+    pkgs.xorg.xcbutilwm
+
+    # zlib (libz.so.1)
+    pkgs.zlib
+
+    # Requirements
+    python.pkgs.tomli
+    python.pkgs.watchdog
+    python.pkgs.wxpython
+    python.pkgs.filelock
+  ];
+
+  propagatedBuildInputs = [
+    python.pkgs.tomli
+    python.pkgs.watchdog
+    python.pkgs.wxpython
+    python.pkgs.filelock
+  ];
+}
